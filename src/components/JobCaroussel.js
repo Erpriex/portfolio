c8fa53ccb0a8be0893cfc23b3ff2c23fdb5d6c1b
Erpriex <erpriex@gmail.com>
2025-05-28 00:46:02 +0200

    💫 Automatic carousel job

--- src/components/JobCaroussel.js
+++ src/components/JobCaroussel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useCallback, useRef } from "react";
 import styles from "../styles/components/JobCaroussel.module.scss";
 import arrowImg from "../assets/img/arrow.png";
 
@@ -14,18 +14,50 @@ const JobCaroussel = () => {
     "DevOps",
   ];
 
-  const [currentJob, setCurrentJob] = React.useState(0);
+  const [currentJob, setCurrentJob] = useState(0);
+  const [isChanging, setIsChanging] = useState(false);
+  const intervalRef = useRef(null);
 
-  const handleClickJob = () => {
-    setCurrentJob((prevJob) => (prevJob + 1) % jobList.length);
-  };
+  const handleJobChange = useCallback(() => {
+    setIsChanging(true);
+    setTimeout(() => {
+      setCurrentJob((prevJob) => (prevJob + 1) % jobList.length);
+      setIsChanging(false);
+    }, 300);
+  }, [jobList.length]);
+
+  const startInterval = useCallback(() => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+    intervalRef.current = setInterval(() => {
+      handleJobChange();
+    }, 6000);
+  }, [handleJobChange]);
+
+  const handleClick = useCallback(() => {
+    handleJobChange();
+    startInterval();
+  }, [handleJobChange, startInterval]);
+
+  useEffect(() => {
+    startInterval();
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, [startInterval]);
 
   return (
     <section className={styles.container}>
       <img className={styles.img} src={arrowImg} alt="arrow" />
       <p className={styles.label}>
         Développeur{" "}
-        <span className={styles.jobLabel} onClick={handleClickJob}>
+        <span
+          className={`${styles.jobLabel} ${isChanging ? styles.leaving : styles.entering}`}
+          onClick={handleClick}
+        >
           {jobList[currentJob]}
         </span>
       </p>
